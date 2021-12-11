d36722ca0a109da324e55616e281539b88fd5997
SupitchaThongkerd <64011640@kmitl.ac.th>
2021-12-11 20:49:31 +0700

    theres a bug need to be fix(fonts)

---
+++ hohohogame.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame == QUIT
+    pygame.display.update
