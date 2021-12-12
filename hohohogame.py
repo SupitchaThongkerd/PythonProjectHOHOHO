d626df190d3429feb37c6b4cb2642dad2c774d77
SupitchaThongkerd <64011640@kmitl.ac.th>
2021-12-12 17:13:59 +0700

    bugs fixed

--- hohohogame.py
+++ hohohogame.py
@@ -1,9 +1,33 @@
-import pygame
+import pygame, sys
+import os
+import time
+import random
 
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
+
+clock = pygame.time.Clock()
+
+from pygame.locals import *
+pygame.init() 
+
+pygame.display.set_caption("HOHOHO GIVE MY SLEIGH BACK!")
+
+window_size = (800, 600)
+
+screen = pygame.display.set_mode(window_size, 0, 32)
+
+santa = pygame.image.load("santa.png")
 
 while True:
+    santa = pygame.image.load("santa.png")
+
+    screen.blit(santa, (50, 50))
+
     for event in pygame.event.get():
-        if event.type == pygame == QUIT
+        if event.type == QUIT:
+            
+            pygame.quit()
+            sys.exit()
+
     pygame.display.update
+    #keep the game running at limited fps
+    clock.tick(60)
