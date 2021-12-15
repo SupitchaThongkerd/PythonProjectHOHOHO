87c08ed2b63315775a2ec3c255cdea1bc94ac517
SupitchaThongkerd <64011640@kmitl.ac.th>
2021-12-15 17:09:26 +0700

    finish mayb

--- hohohogame.py
+++ hohohogame.py
@@ -5,6 +5,7 @@ import random
 
 from pygame import draw
 from pygame.constants import WINDOWHITTEST
+from pygame.sprite import collide_circle
 pygame.font.init()
 os.chdir("/Users/pearsupitcha/Documents/python project/PythonProjectHOHOHO")
 
@@ -32,21 +33,44 @@ bhudda2 = pygame.transform.scale(bhudda, (45, 60))
 
 #santa's candycane
 candycane = pygame.image.load(os.path.join("/Users/pearsupitcha/Documents/python project/PythonProjectHOHOHO", "candycane.png"))
-candycane2 = pygame.transform.scale(candycane, (20, 20))
+candycane2 = pygame.transform.scale(candycane, (30, 30))
 #grinch's tomato
 tomato = pygame.image.load(os.path.join("/Users/pearsupitcha/Documents/python project/PythonProjectHOHOHO", "tomato.png"))
-tomato2 = pygame.transform.scale(tomato, (20, 20))
+tomato2 = pygame.transform.scale(tomato, (30, 30))
 #pumpkin's fire
 fire = pygame.image.load(os.path.join("/Users/pearsupitcha/Documents/python project/PythonProjectHOHOHO", "fire.png"))
-fire2 = pygame.transform.scale(fire, (20, 20))
+fire2 = pygame.transform.scale(fire, (30, 30))
 #bhudda's lotus
 lotus = pygame.image.load(os.path.join("/Users/pearsupitcha/Documents/python project/PythonProjectHOHOHO", "lotus.png"))
-lotus2 = pygame.transform.scale(bhudda, (60, 60))
+lotus2 = pygame.transform.scale(lotus, (30, 30))
 
 #background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("/Users/pearsupitcha/Documents/python project/PythonProjectHOHOHO", "pjbg.png")), (WIDTH, HEIGHT))
+BG2 = pygame.transform.scale(pygame.image.load(os.path.join("/Users/pearsupitcha/Documents/python project/PythonProjectHOHOHO", "cmbg.jpg")), (WIDTH, HEIGHT))
+
+class Magic:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+    
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+    
+    def move(self, velocity):
+        self.x += velocity
+
+    def off_screen(self, WIDTH):
+        return not(self.x <= WIDTH and self.x >= 0)
+    
+    def collision(self, object):
+        return collide(self, object)
 
 class Characters:
+    #FPS is 60
+    COOLDOWN = 20
+
     def __init__(self, x, y, health = 100):
         self.x = x
         self.y = y
@@ -58,7 +82,33 @@ class Characters:
 
     def draw(self, window):
         window.blit(self.characters_img, (self.x, self.y))
+        for magic in self.magics:
+            magic.draw(window)
     
+    #move laser and check collision
+    def move_magics(self, velocity, object):
+        self.cooldown()
+        for magic in self.magics:
+            magic.move(velocity)
+            if magic.off_screen(WIDTH):
+                self.magics.remove(magic)
+            elif magic.collision(object):
+                object.health -= 10
+                self.magics.remove(magic)
+
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
+
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            magic = Magic(self.x, self.y, self.magic_img)
+            self.magics.append(magic)
+            #set cool down counter to count up
+            self.cool_down_counter = 1
+
     #make the picture of the player not go off the screen
     def get_width(self):
         return self.characters_img.get_width()
@@ -70,12 +120,35 @@ class Player(Characters):
     def __init__(self, x, y, health = 100):
         super().__init__(x, y, health)
         self.characters_img = santa2
-        self.magic_img = candycane
+        self.magic_img = candycane2
         #make the mask of the player to tell wheres the pixels are
         #so when theres a collition we will know whether it hits the pixels or not
         self.mask = pygame.mask.from_surface(self.characters_img)
         self.max_health = health
 
+    def move_magics(self, velocity, objects):
+        self.cooldown()
+        for magic in self.magics:
+            magic.move(velocity)
+            if magic.off_screen(WIDTH):
+                self.magics.remove(magic)
+            else: 
+                for object in objects:
+                    if magic.collision(object):
+                        objects.remove(object)
+                        if magic in self.magics:
+                            self.magics.remove(magic)
+
+    def draw(self, window):
+        super().draw(window)
+        self.healthbar(window)
+
+    def healthbar(self, window):
+        pygame.draw.rect(window, (166, 8, 95), (self.x, self.y + self.characters_img.get_height() + 10, self.characters_img.get_width(), 10))
+        #self.max_health(health rn)- self.health(full health))/self.max_health got the percent health loss
+        pygame.draw.rect(window, (35, 96, 95), (self.x, self.y + self.characters_img.get_height() + 10, self.characters_img.get_width() * (self.health /self.max_health), 10))
+    
+    
 class Enemy(Characters):
     COLOR_MAP = {
                 "red": (grinch2, tomato2),
@@ -90,21 +163,35 @@ class Enemy(Characters):
     def move(self, velocity):
         self.x -= velocity
 
+        def shoot(self):
+            if self.cool_down_counter == 0:
+                magic = Magic(self.x, self.y - 10, self.magic_img)
+                self.magics.append(magic)
+                #set cool down counter to count up
+                self.cool_down_counter = 1
+
+def collide(object1, object2):
+    offset_x = object2.x - object1.x
+    offset_y = object2.y - object1.y
+    return object1.mask.overlap(object2.mask, (offset_x, offset_y)) != None
+
+
 def main():
     run = True
     FPS = 60
     level1 = 0
     lives = 5
 
-    main_font = pygame.font.SysFont("8-BIT WONDER.TTF", 40)
-    lost_font = pygame.font.SysFont("8-BIT WONDER.TTF", 50)
+    main_font = pygame.font.Font("/Users/pearsupitcha/Documents/python project/PythonProjectHOHOHO/FFF_Tusj.ttf", 40)
+    lost_font = pygame.font.Font("/Users/pearsupitcha/Documents/python project/PythonProjectHOHOHO/Quinquefive.ttf", 21)
 
     enemies = []
     #wavelemgth =5  = 10 enemies if 6 then +1
-    wave_length = 5
-    enemies_velocity = 1
+    wave_length = 3
+    enemies_velocity = 2
 
-    player_velocity = 7 #lower clock speed = nedd higher velocity
+    player_velocity = 6 #lower clock speed = nedd higher velocity
+    magic_velocity = 4
 
     player = Player(10, 300)
 
@@ -117,8 +204,8 @@ def main():
     def redraw_window():
         win.blit(BG, (0, 0))
         #draw text
-        lives_label = main_font.render(f"Lives: {lives}", 1, (0, 0, 0))
-        level1_label = main_font.render(f"Level: {level1}", 1, (0, 0, 0))
+        lives_label = main_font.render(f"Lives: {lives}", 1, (13, 36, 0))
+        level1_label = main_font.render(f"Level: {level1}", 1, (13, 36, 0))
         
         win.blit(lives_label, (10, 10))
         win.blit(level1_label,(WIDTH - level1_label.get_width() - 10, 10))
@@ -128,8 +215,9 @@ def main():
 
         player.draw(win)
 
+
         if lost:
-            lost_label = lost_font.render("YOU CAN'T SAVE SANTA's SLEIGH!", 1, (255, 0, 0))
+            lost_label = lost_font.render("YOU FAILED TO SAVE SANTA'S SLEIGH:(", 1, (255, 0, 0))
             win.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 325))
 
         pygame.display.update()
@@ -137,6 +225,8 @@ def main():
     #run/check 60 times every second
     while run:
         clock.tick(FPS)
+
+        redraw_window()
 
         if lives <= 0 or player.health <= 0:
             lost = True
@@ -175,14 +265,46 @@ def main():
         #moving down and not off the screen height
         if keys[pygame.K_s] and player.y + player_velocity + player.get_height() < HEIGHT:
             player.y += player_velocity
-        
+        #enter space to shoot
+        if keys[pygame.K_SPACE]:
+            player.shoot()
+
         for enemy in enemies[:]:
             #make the enemies move
             enemy.move(enemies_velocity)
+            enemy.move_magics(-magic_velocity, player)
+
+            #every 2sec enemy will shoot
+            if random.randrange(0, 2*60) == 1:
+                enemy.shoot()
+
+            #player health decrease after the collision
+            if collide(enemy, player):
+                player.health -= 10
+                enemies.remove(enemy)
             #deducting lives if the enemy off the screen
-            if enemy.x + enemy.get_width() < WIDTH-951:
+            elif enemy.x + enemy.get_width() < WIDTH-951:
                 lives -= 1
                 enemies.remove(enemy)
 
-        redraw_window()
-main()
+
+        #check if the magic collide with any of the enemies
+        player.move_magics(magic_velocity, enemies)
+
+def main_menu():
+    title_font = pygame.font.Font("/Users/pearsupitcha/Documents/python project/PythonProjectHOHOHO/Quinquefive.ttf", 20)
+    run = True
+    while run:
+        win.blit(BG2, (0,0))
+        title_label = title_font.render("Click to help Santa get his sleigh back!", 1, (0, 0, 0))
+        win.blit(title_label,(WIDTH/2 - title_label.get_width()/2, 350))
+
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                main()
+    pygame.quit()
+
+main_menu()
